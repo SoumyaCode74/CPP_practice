f13c71f907613dd09e41b9c29c566d80dcfc9e20
SoumyaCode74 <100553212+SoumyaCode74@users.noreply.github.com>
2024-07-10 14:49:15 +0000

    first friend function example

--- Cpp_CodeWithHarry/friend_fcns/friendfcn_lec26.h
+++ Cpp_CodeWithHarry/friend_fcns/friendfcn_lec26.h
@@ -5,14 +5,14 @@ using namespace std;
 class Complex{
         float a;
         float b;
-    public:
-        void setNumber(float, float);
-        void printNumber(void);
         /*
             * the following function is not a method of the class
             * yet the function can "use" the private members of the class ("use" means get or set the members)
         */
-        friend Complex sumComplex(Complex, Complex);
+        friend Complex sumComplex(Complex, Complex);        
+    public:
+        void setNumber(float, float);
+        void printNumber(void);
 };
 
 Complex sumComplex(Complex, Complex);
